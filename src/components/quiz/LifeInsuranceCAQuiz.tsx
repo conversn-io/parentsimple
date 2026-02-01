488bd344e6df5fa20f56647e3acd10c81489aff1
conversn <keenan@keenanshaw.com>
2026-01-31 21:28:02 -0800

    fix: Add missing Link import for Terms/Privacy links

--- src/components/quiz/LifeInsuranceCAQuiz.tsx
+++ src/components/quiz/LifeInsuranceCAQuiz.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/navigation'
+import Link from 'next/link'
 import Image from 'next/image'
 import {
   LIFE_INSURANCE_CA_STEPS,
